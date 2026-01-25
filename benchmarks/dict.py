d8b7360c41c4e8e1ed7ee66f83422e9eda7c3c92
Jing, Mason (M.) <y38jing@uwaterloo.ca>
2026-01-25 16:34:02 -0500

    refactor dict.py to measure performance of dictionary entry additions

--- benchmarks/dict.py
+++ benchmarks/dict.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
-# Number to guess: How many entries can
-# we add to a dictionary of a fixed maximum size
-# in a second?
+# Number to guess: How many entries can we add to a dictionary of a fixed maximum size in a second?
 
-# Note: we take `i % 1000` to control
-# the size of the dictionary
+# Iterations: 50,000,000
+# Time: 2.487682 seconds
+# Rate: 20,099,035 iterations/second
+
+# Note: we take `i % 1000` to control the size of the dictionary
+
+
+import time
 
 def f(NUMBER):
     d = {}
@@ -16,4 +20,15 @@ def f(NUMBER):
 if __name__ == '__main__':
     import sys
 
-    f(int(sys.argv[1]))
+    iterations = int(sys.argv[1])
+
+    start = time.perf_counter()
+    f(iterations)
+    end = time.perf_counter()
+
+    elapsed = end - start
+    rate = iterations / elapsed
+
+    print(f"Iterations: {iterations:,}")
+    print(f"Time: {elapsed:.6f} seconds")
+    print(f"Rate: {rate:,.0f} iterations/second")
