8f8b4b61f6c47db802425744801b5608a5f0dc9a
Jing, Mason (M.) <y38jing@uwaterloo.ca>
2026-02-20 16:39:16 -0500

    refactor: enhance array allocation and performance measurement in fill_array_out_of_order.c

--- benchmarks/fill_array_out_of_order.c
+++ benchmarks/fill_array_out_of_order.c
@@ -1,25 +1,54 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
-// Number to guess: How big of an array (in bytes)
-// can we allocate and fill with 5s in a second?
+// Number to guess: How big of an array (in bytes) can we allocate and fill in a second?
 // The catch: We do it out of order instead of in order.
-int main(int argc, char** argv) {
-    int NUMBER, i;
-    NUMBER = atoi(argv[1]);
 
+// Bytes: 100,000,000
+// Time: 1.214000 seconds
+// Rate: 82,372,323 bytes/second
+
+
+char* f(const long long NUMBER) {
     char* array = malloc(NUMBER);
-    int j = 1;
-    for (i = 0; i < NUMBER; ++i) {
-        j = j * 2;
-        if (j > NUMBER) {
-            j = j - NUMBER;
-        }
+
+    long long j = 1;
+    for (long long i = 0; i < NUMBER; ++i) {
+        j *= 2;
+        j = j > NUMBER ? j - NUMBER : j;
         array[j] = j;
     }
 
-    printf("%d", array[NUMBER / 7]);
     // so that -O2 doesn't optimize out the loop
+    volatile char result = array[NUMBER / 7];
+
+    return array;
+}
+
+int main(int argc, char** argv) {
+    char* endptr;
+    errno = 0;
+    const long long iterations = strtoll(argv[1], &endptr, 10);
+
+    // Check for conversion errors
+    if (errno != 0 || *endptr != '\0' || endptr == argv[1]) {
+        fprintf(stderr, "Error: Invalid number '%s'\n", argv[1]);
+        return 1;
+    }
+
+    const clock_t start = clock();
+    char* array = f(iterations);
+    const clock_t end = clock();
+
+    const double elapsed = (double)(end - start) / CLOCKS_PER_SEC;
+    const double rate = (double)iterations / elapsed;
+
+    printf("Bytes: %lld\n", iterations);
+    printf("Time: %.6f seconds\n", elapsed);
+    printf("Rate: %.0f bytes/second\n", rate);
+
+    free(array);
 
     return 0;
 }
