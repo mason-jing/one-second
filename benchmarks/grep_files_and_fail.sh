5afb2b998f64a96744f472b13c6151cd025ef178
Jing, Mason (M.) <y38jing@uwaterloo.ca>
2026-02-20 23:24:59 -0500

    refactor: improve variable assignment and quoting in grep_files_and_fail.sh

--- benchmarks/grep_files_and_fail.sh
+++ benchmarks/grep_files_and_fail.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
-# Number to guess: How many files can `grep`
-# search, unsuccessfully, in a second?
-# Note: the file contents will be in the filesystem cache.
+# Number to guess: How many files can `grep` search, unsuccessfully, in a second?
+# Note: the files will be in the filesystem cache.
 
-NUMBER=$1
 
-grep -r --files-without-match --binary-files=without-match pandapandapandapanda /usr 2> /dev/null | head -n $NUMBER > /dev/null 
+NUMBER="$1"
+
+grep -r --files-without-match --binary-files=without-match pandapandapandapanda /usr 2> /dev/null | head -n "$NUMBER" > /dev/null
